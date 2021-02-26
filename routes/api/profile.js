576538f7bac2cd39079b103a8029916e52da49da
Karsenh <KarsenHansen@gmail.com>
2021-02-26 00:47:21 -0800

    Updated profile routes.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -14,20 +18,63 @@ const User = require('../../models/User');
  */
 router.get('/me', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate(
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate(
             'user',
             ['name', 'avatar']
         );
 
-        if(!profile) {
-            return res.status(400).json({ msg: 'There is no profile for this user' });
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user'
+            });
         }
 
         res.json(profile);
-    } catch(err) {
+    } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
     }
 });
+
+/**
+ * @ROUTE   POST (API/Profile)
+ * @desc    Create or Update user profile
+ * @access  Private
+ * 
+ */
+router.post("/", auth,
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is requried').not().isEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            company,
+            website,
+            location,
+            bio,
+            status,
+            githubusername,
+            skills,
+            youtube,
+            facebook,
+            twitter,
+            instagram,
+            linkedin
+        } = req.body;
+
+        // Build the Profile Object
+        const profileFields = {};
+        profileFields.user = req.user.id;
+        if(company) profileFields.company = company
+    }
+);
 
 module.exports = router;
