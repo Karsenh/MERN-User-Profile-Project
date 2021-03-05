643f4d3709414920d581c0aeba48185e82905db0
Karsenh <KarsenHansen@gmail.com>
2021-03-04 23:36:27 -0800

    Created Posts routes for users to create (post) & delete their own comments on posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -189,4 +189,86 @@ router.put('/unlike/:id', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+/**
+ * @ROUTE   POST (api/posts/comment/:id)
+ * @desc    Comment on a post
+ * @access  Private
+ */
+router.post('/comment/:id', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], 
+async (req, res) => {
+    const errors = validationResult(req);
+
+    // Check for errors
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        // Get the post by id from the url
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        // Send that new comment variable in a response
+        res.json(post.comments);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @ROUTE   DELETE (api/posts/comment/:id/:comment_id)
+ * @desc    Delete comment by post and comment id
+ * @access  Private
+ */
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        // Get the post by its ID from the url
+        const post = await Post.findById(req.params.id);
+
+        // Get the comment within the post by ID
+        // Will either return the comment or false
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure the comment exists
+        if(!comment) {
+            // Return res.status 404 not found
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+
+        // Ensure the user trying to delete the comment is the user that created the comment
+        if(comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        // Locate index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+        
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
