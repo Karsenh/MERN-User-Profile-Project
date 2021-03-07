209a9fceadc658f38589118825eae7129004ea3d
Karsenh <KarsenHansen@gmail.com>
2021-03-06 21:42:37 -0800

    Added React client and connected routes to landing and navbar

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>MERN User Template</h1>
+          <p className='lead'>
+            This is a MERN Stack web application for CPSC-310
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
