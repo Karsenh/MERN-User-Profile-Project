2fbb48877a9160e781e7954bc2957d32ec535d73
Karsenh <KarsenHansen@gmail.com>
2021-03-07 16:08:15 -0800

    Added Private Route to protect and redirect against unauthenticated access

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+/**
+ * Fetch all of our data using an action
+ * bring it in from redux state
+ * pass it down to other components (i.e. experience and education components)
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = props => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
