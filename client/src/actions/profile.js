0757f46b9e4fe46d59916ac6d654069c63baf60d
Karsenh <KarsenHansen@gmail.com>
2021-03-15 01:50:42 -0700

    Can now create a user Profile on the front-end

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -22,3 +22,48 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update user profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    // making post request to api/profile
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      // Payload is actual profile send over
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    // If editing - stay on page | if creating - redirect
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.data.msg,
+        status: err.response.status,
+      },
+    });
+  }
+};
